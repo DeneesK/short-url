ad77c830b6834a422190b6257e5c9293c6be970d
MacBook <macbook@MacBook-Pro.local>
2024-12-20 10:50:30 +0100

    iter1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/DeneesK/short-url/internal/app/repository"
+	"github.com/DeneesK/short-url/internal/app/router"
+)
+
+func main() {
+	s := repository.NewRepository()
+	r := router.NewRouter(s)
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
+}
